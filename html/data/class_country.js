0c65aac7bc558f0035328113b821af65926bfeef
anprieur <antoine.prieur@yahoo.fr>
2025-03-12 12:01:46 +0100

    Update class_country.js

--- html/data/class_country.js
+++ html/data/class_country.js
@@ -1,23 +1,28 @@
 class Country {
     static all_countries={};
 
-    constructor (c, n, cap, cont, pop)
+    constructor (c, n, cap, cont, pop, vois)
     {
         this._code=c;
         this._nom=n;
         this._capital=cap;
         this._continent=cont;
         this._population=pop;
-        this._voisins=[];
+        this._voisins=vois;
     }
 
     toString() { return `${this._code}, ${this._nom}, ${this._capital}, ${this._continent}, ${this._population} hab, (${this._voisins})`}
 
     static fill_countries() {
         countries.forEach(country => {
-            Country.all_countries[language["iso639_2"]] = new Country(
-                    language["iso639_2"], 
-                    language["name"]);
+            Country.all_countries[country["alpha3Code"]] = new Country(
+                    country["alpha3Code"], 
+                    country["name"],
+                    country["capital"],
+                    country["region"],
+                    country["population"],
+                    country["borders"]
+            );
         });
     }
 
@@ -25,4 +30,4 @@ class Country {
 
 Country.fill_countries();
 console.table(Country.all_countries);
-console.log(Country.all_countries["pus"].toString());
+console.log(Country.all_countries["AFG"].toString());
